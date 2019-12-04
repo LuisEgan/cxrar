d3303ca1512ff341a4c69dc8e66d9bc3a8a57bb5
Luis Daniel <legan@cleverit.cl>
2019-12-04 18:52:47 -0300

    point camera message

---
+++ scanMessage.js
@@ -0,0 +1,32 @@
+function docReady(fn) {
+  // see if DOM is already available
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+docReady(function() {
+  cancelAnimationFrame.registerComponent("registerEvents", {
+    init: function() {
+      var marker = this.el;
+      var message = document.getElementById("message");
+
+      marker.setAttribute("emitevents", "true");
+
+      marker.addEventListener("markerFound", function() {
+        message.style.display = "none";
+        alert("found!");
+      });
+
+      marker.addEventListener("markerLost", function() {
+        message.style.display = "block";
+      });
+    }
+  });
+});
